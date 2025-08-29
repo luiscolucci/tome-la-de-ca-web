3382a9b5de2a29c98d7641499852f1fc1046d77b
luiscolucci <colucci.luis@gmail.com>
2025-08-29 17:28:17 -0300

    Deploy

--- backend/src/services/firebase.js
+++ backend/src/services/firebase.js
@@ -1,19 +1,24 @@
-// backend/src/services/firebase.js
-
 const admin = require("firebase-admin");
 const path = require("path");
 
-const serviceAccountPath = path.join(
-  process.cwd(),
-  process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH
-);
+// VERIFICA SE ESTAMOS NO AMBIENTE DE PRODUÇÃO (CLOUD RUN)
+if (process.env.NODE_ENV === "production") {
+  // Na nuvem, o SDK do Firebase encontra as credenciais automaticamente.
+  // Nenhuma configuração extra é necessária.
+  admin.initializeApp();
+} else {
+  // No nosso ambiente local (desenvolvimento), carregamos a chave a partir do ficheiro .env.
+  const serviceAccountPath = path.join(
+    process.cwd(),
+    process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH
+  );
+  const serviceAccount = require(serviceAccountPath);
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
-const serviceAccount = require(serviceAccountPath);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
+// Exporta os serviços de Autenticação e do Banco de Dados para serem usados noutras partes do backend
 const auth = admin.auth();
 const db = admin.firestore();
 
